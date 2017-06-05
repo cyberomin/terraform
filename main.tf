35674b6676937e47cc3734af2652b8d5faca2280
Celestine Omin <cyberomin@yahoo.com>
2017-06-05 10:26:00 +0100

    Formatted code

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 variable "token" {
-    default = "xxxxxx-xxx-xxx-xxxxx"
+  default = "xxxxxx-xxx-xxx-xxxxx"
 }
 
 provider "digitalocean" {
-    token = "${var.token}"
+  token = "${var.token}"
 }
 
 resource "digitalocean_ssh_key" "default" {
-    name        = "SSH Key Credential"
-    public_key  = "${file("/home/vagrant/.ssh/id_rsa.pub")}"
+  name       = "SSH Key Credential"
+  public_key = "${file("/home/vagrant/.ssh/id_rsa.pub")}"
 }
 
 resource "digitalocean_droplet" "web" {
-    image   = "ubuntu-16-04-x64"
-    name    = "web-1"
-    region  = "lon1"
-    size    = "1gb"   
+  image  = "ubuntu-16-04-x64"
+  name   = "web-1"
+  region = "lon1"
+  size   = "1gb"
 }
 
 output "ip" {
-    value = "${digitalocean_droplet.web.ipv4_address}"
+  value = "${digitalocean_droplet.web.ipv4_address}"
 }
